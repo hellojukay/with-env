ab0aabad1b139552ff367b27445f1219844ae1be
hellojukay <hellojukay@163.com>
2022-03-08 10:39:01 +0800

    add build script

---
+++ build.pl
@@ -0,0 +1,20 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+sub command_exsits {
+    my $cmd = shift;
+    my $exit = system("which $cmd > /dev/null 2>&1");
+    return $exit == 0;
+}
+
+$ENV{CGO_ENABLED} = 0;
+if(command_exsits('gox')) {
+    print("[INFO] gox command found , now run gox build.\n");
+    my $arch="darwin/arm64 darwin/amd64 linux/386 linux/amd64 windows/amd64 windows/386";
+    system("gox	-osarch=\"$arch\"  -output=\"dist/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
+} else {
+    print("[WARN] gox command not founnd, runing go build.\n");
+    system("go build");
+}
