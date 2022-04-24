e61a7f3203acd8138eca2d414de3bb385d066f85
licong <licong@qianxin.com>
2022-04-24 06:54:13 +0000

    add --version flag

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime/debug"
 
 	"github.com/joho/godotenv"
+)
+
+var (
+	showVersion = flag.Bool("version", false, "pring progream version")
 )
 
 // first load env from ~/.env
 // second load env from .env
 func init() {
+	flag.Parse()
+	if *showVersion {
+		PrintVersion()
+		os.Exit(0)
+	}
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: with-env {command}\n")
+		os.Exit(1)
+	}
 	home, _ := os.UserHomeDir()
 	loadEnv(filepath.Join(home, ".env"), ".env")
 }
@@ -31,5 +46,12 @@ func main() {
 	if err := c.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%q\n", err)
 		os.Exit(1)
+	}
+}
+
+func PrintVersion() {
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		println(info.Main.Version, info.Main.Sum)
 	}
 }
